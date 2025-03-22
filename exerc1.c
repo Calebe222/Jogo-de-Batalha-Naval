5878ba9785b848e55bb395cd09d81d28e72413f9
Calebe222 <136852396+Calebe222@users.noreply.github.com>
2025-03-22 01:35:30 +0000

    adição de novos exercícios

--- exerc1.c
+++ exerc1.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
-int main () {
-    int numero[5] = {10, 20, 30, 40, 50}; // Inicialização do array com valores 
-
-
-    // Acessando os valores e imprimindo os elemento específicos
-    printf("O primeiro elemento é: %d\n, numero[0]");
-    printf("o Segundo elemento é: %d\n, numero[2]");
-    printf("O terceiro elmemento é: %d\n, numero[4]");
-
+ 
+int main() {
+    int numeros[5] = {10, 20, 30, 40, 50};  // Inicialização do array com valores
+ 
+    // Acessando e imprimindo elementos específicos do array
+    printf("O primeiro elemento é %d\n", numeros[0]);
+    printf("O terceiro elemento é %d\n", numeros[2]);
+    printf("O quinto elemento é %d\n", numeros[4]);
+ 
     return 0;
 }
---
+++ exerc2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+    float notas[3] = {75.5, 80.3, 90.5}; // Array de notas dos 3 alunos
+
+    printf("A nota do primeiro aluno é %.1f\n", notas[0]);
+    printf("A nota do segundo aluno é %.1f\n", notas[1]);
+    printf("A nota do segundo aluno é %.1f\n", notas[2]);
+
+
+}
