d662df319981f95f3b4be8b45c1bd520702eec61
Calebe222 <136852396+Calebe222@users.noreply.github.com>
2025-03-22 01:23:24 +0000

    adição do primeiro teste de Array

---
+++ exerc1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+    int numero[5] = {10, 20, 30, 40, 50}; // Inicialização do array com valores 
+
+
+    // Acessando os valores e imprimindo os elemento específicos
+    printf("O primeiro elemento é: %d\n, numero[0]");
+    printf("o Segundo elemento é: %d\n, numero[2]");
+    printf("O terceiro elmemento é: %d\n, numero[4]");
+
+    return 0;
+}
